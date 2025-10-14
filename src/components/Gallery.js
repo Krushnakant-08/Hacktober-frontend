623cc0ce4fc86ca6e0fbf7ff0e70e5b3f0a2ea56
Krushnakant Patil <krushnakantpatil06@gmail.com>
2025-10-14 15:02:51 +0530

    gallery folder structure improvised

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -166,7 +166,7 @@ const Gallery = () => {
             eventImageSets.map((event) => {
               const images = event.files.map((filename, index) => ({
                 id: `${event.eventId}-${index + 1}`,
-                src: `/assets/gallery/${filename}`,
+                src: `/assets/gallery/2k24/${filename}`,
                 alt: `${event.altPrefix} ${index + 1}`,
                 title: event.title,
                 description: event.description
